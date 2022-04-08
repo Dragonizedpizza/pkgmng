22c35e5531927bbb91a715cf7d07774230b96de7
Dragonizedpizza <dragon@dragonizedpizza.com>
2022-04-08 20:37:20 +0200

    format

--- src/constants.ts
+++ src/constants.ts
@@ -1,37 +1,36 @@
 export const Regexes = {
-    RemoveServerEquals: /^Server = (.*)$/,
+	RemoveServerEquals: /^Server = (.*)$/,
 };
 
 export function GlobalRegex(regex: RegExp): RegExp {
-    return new RegExp(regex.source, regex.flags + "g");
-};
+	return new RegExp(regex.source, regex.flags + "g");
+}
 
 export const PacmanFlags = {
-    NoDependencyChecks: "--nodeps",
-    databasePath: "--dbpath",
-    root: "--root",
-    search: "--search",
-    verbose: "--verbose",
-    arch: "--arch",
-    asDependencies: "--asdeps",
-    asExplicit: "--asexplicit",
-    assumeInstalled: "--assume-installed",
-    cacheDir: "--cachedir",
-    color: "--color",
-    config: "--config",
-    debug: "--debug",
-    databaseOnly: "--dbonly",
-    disableDownloadTimeout: "--disable-download-timeout",
-    gpgDirectory: "--gpgdir",
-    hookDirectory: "--hookdir",
-    ignorePackages: "--ignore",
-    ignoreGroups: "--ignoregroups",
-    logFile: "--logfile",
-    noReinstallUptoDate: "--nodeps",
-    noScriptlet: "--noscriptlet",
-    printFormat: "--print-format",
-    systemRoot: "--sysroot",
-    refresh: "--refresh",
-    forceRefresh: "-yy",
-    
+	NoDependencyChecks: "--nodeps",
+	databasePath: "--dbpath",
+	root: "--root",
+	search: "--search",
+	verbose: "--verbose",
+	arch: "--arch",
+	asDependencies: "--asdeps",
+	asExplicit: "--asexplicit",
+	assumeInstalled: "--assume-installed",
+	cacheDir: "--cachedir",
+	color: "--color",
+	config: "--config",
+	debug: "--debug",
+	databaseOnly: "--dbonly",
+	disableDownloadTimeout: "--disable-download-timeout",
+	gpgDirectory: "--gpgdir",
+	hookDirectory: "--hookdir",
+	ignorePackages: "--ignore",
+	ignoreGroups: "--ignoregroups",
+	logFile: "--logfile",
+	noReinstallUptoDate: "--nodeps",
+	noScriptlet: "--noscriptlet",
+	printFormat: "--print-format",
+	systemRoot: "--sysroot",
+	refresh: "--refresh",
+	forceRefresh: "-yy",
 };
--- src/sources/pacman.ts
+++ src/sources/pacman.ts
@@ -5,21 +5,32 @@ import { PacmanFlags } from "../constants";
 export const Pacman = {
 	async install(packages: string[], options: PacmanInstallOptions) {
 		let query = ["pacman", "-S"],
-			addFlag = ({ k, v, e = true }: { k: string; v?: string; e?: any } = {} as any) => {
-                if (!e) return;
-                
-                let newQuery = k;
-                if (v) newQuery += `=${v}`;
+			addFlag = (
+				{
+					k,
+					v,
+					e = true,
+				}: { k: string; v?: string; e?: any } = {} as any,
+			) => {
+				if (!e) return;
 
-                query.push(newQuery);
+				let newQuery = k;
+				if (v) newQuery += `=${v}`;
 
+				query.push(newQuery);
 			};
 
-		addFlag({ k: PacmanFlags.databasePath, v: options.databasePath, e: options.databasePath });
-        addFlag({ k: PacmanFlags.NoDependencyChecks, e: options.noDependencyChecks });
-        addFlag({ k: PacmanFlags.root, v: options.root, e: options.root });
+		addFlag({
+			k: PacmanFlags.databasePath,
+			v: options.databasePath,
+			e: options.databasePath,
+		});
+		addFlag({
+			k: PacmanFlags.NoDependencyChecks,
+			e: options.noDependencyChecks,
+		});
+		addFlag({ k: PacmanFlags.root, v: options.root, e: options.root });
 
-
-        exec(query.join(" "));
+		exec(query.join(" "));
 	},
 };
